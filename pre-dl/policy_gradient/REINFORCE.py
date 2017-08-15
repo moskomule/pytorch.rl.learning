bc1ce29f954fb74b39225bc8742d48d34c65d9a6
moskomule <hataya@keio.jp>
2017-08-15 23:57:40 +0900

    added leaning rate scheduler

--- pre-dl/policy_gradient/REINFORCE.py
+++ pre-dl/policy_gradient/REINFORCE.py
@@ -2,14 +2,17 @@ from random import choices
 import torch
 from torch import Tensor
 from torch import from_numpy as to_tensor
+
 from policy_gradient.pg_base import PGBase
 
 
 class REINFORCE(PGBase):
-    def __init__(self, env_name, num_episodes=10000, alpha=0.9, gamma=0.9):
-        super(REINFORCE, self).__init__(env_name, num_episodes, alpha, gamma, policy="softmax_policy", report_freq=500)
+    def __init__(self, env_name, num_episodes=10000, alpha=0.9, gamma=0.9, decay_freq=1000):
+        super(REINFORCE, self).__init__(env_name, num_episodes, alpha, gamma, policy="softmax_policy", report_freq=500,
+                                        decay_freq=decay_freq)
         self._feature = Tensor(self.action_size, self.obs_size)
         self.actions = range(self.action_size)
+        self.min_alpha = 0.1
 
     def _loop(self):
         done = False
@@ -30,6 +33,10 @@ class REINFORCE(PGBase):
         # update weight
         self.weight = weight
         return total_reward
+
+    def schedule_alpha(self, episode):
+        if self.alpha > self.min_alpha and episode % self.decay_freq == 0 and episode != 0:
+            self.alpha = self.alpha / (episode / self.decay_freq)
 
     @property
     def softmax(self):
